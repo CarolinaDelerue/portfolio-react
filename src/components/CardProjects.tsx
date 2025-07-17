f86b5ad3fae00315a55a95001ca514551e5d5639
Carolina Delerue <delerue.carolina@gmail.com>
2025-07-17 15:46:46 +0200

    fix TS cardproject

--- src/components/CardProjects.tsx
+++ src/components/CardProjects.tsx
@@ -2,7 +2,9 @@ import { FaHtml5, FaCss3Alt, FaReact } from 'react-icons/fa'
 import { SiNuxtdotjs, SiTailwindcss, SiFigma } from 'react-icons/si'
 import { VscMarkdown } from 'react-icons/vsc'
 
+
 type IconTags = 'HTML' | 'CSS' | 'React' | 'Nuxt' | 'markdown' | 'Tailwind CSS' | 'Figma';
+
 
 const iconMap: Record<IconTags, JSX.Element> = {
   HTML: <FaHtml5 className="text-xl text-white md:text-2xl" />,
@@ -19,7 +21,7 @@ interface CustomCardProps {
   title: string
   description: string
   challenge?: string
-  tags: IconTags[]
+  tags: string[]
   linkIcon?: JSX.Element | string
   linkUrl: string
 }
@@ -47,20 +49,18 @@ const CustomCard: React.FC<CustomCardProps> = ({
           <p className="text-sm text-gray-400 md:text-lg">{description}</p>
           {challenge && <p className="mt-1 text-xs text-gray-500 md:text-sm">{challenge}</p>}
           <div className="flex justify-center mt-3 space-x-2 md:justify-start">
-            {tags.map((tag, index) => (
-              iconMap[tag] ? (
-                <span key={index}>
-                  {iconMap[tag]}
-                </span>
-              ) : null
-            ))}
+            {tags.map((tag, index) => {
+              const icon = iconMap[tag as IconTags];
+              return icon ? <span key={index}>{icon}</span> : null;
+            })}
           </div>
         </div>
 
         <div className="flex-shrink-0 text-center transition-opacity duration-300 opacity-0 md:text-right group-hover:opacity-100">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M19 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h6v2H5v12h12v-6zM13 3v2h4.586l-7.793 7.793l1.414 1.414L19 6.414V11h2V3z" /></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path fill="white" d="M19 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h6v2H5v12h12v-6zM13 3v2h4.586l-7.793 7.793l1.414 1.414L19 6.414V11h2V3z" />
+          </svg>
         </div>
-
       </div>
     </a>
   )
